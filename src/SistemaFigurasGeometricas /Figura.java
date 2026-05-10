474f3d8d0c2f6fc0c0b5a11374d4c446c467a5ac
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-05-10 16:53:21 -0500

    Add abstract class Figura with area and perimeter methods

--- src/SistemaFigurasGeometricas /Figura.java
+++ src/SistemaFigurasGeometricas /Figura.java
@@ -1,1 +1,22 @@
+public abstract class Figura {
+    protected String color;
+    protected boolean rellena;
 
+    public Figura(String color, boolean rellena) {
+        this.color = color;
+        this.rellena = rellena;
+    }
+
+    abstract double calcularArea();
+    abstract double calcularPerimetro();
+    public void describir(){
+        System.out.println("Color: " + color);
+        if(rellena){
+            System.out.println("Esta rellena");
+        } else {
+            System.out.println("No esta rellena");
+        }
+        System.out.println("Area: " + calcularArea());
+        System.out.println("Perimetro: " + calcularPerimetro());
+    }
+}
