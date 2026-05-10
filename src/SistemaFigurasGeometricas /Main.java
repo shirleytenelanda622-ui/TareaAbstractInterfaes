3357120edd089fa87a831aec9475c5d38405281b
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-05-10 16:54:46 -0500

    Update Main.java

--- src/SistemaFigurasGeometricas /Main.java
+++ src/SistemaFigurasGeometricas /Main.java
@@ -1,1 +1,13 @@
-
+public class Main {
+    public static void main(String[] args) {
+        Circulo c = new Circulo("Morado", true, 5);
+        Rectangulo r = new Rectangulo("Rojo", false, 6, 4);
+        Triangulo t = new Triangulo("Amarillo", true, 5, 6);
+        Figura[] figuras = {c, r, t};
+        for(Figura figura : figuras){
+            figura.describir();
+            figura.calcularArea();
+            figura.calcularPerimetro();
+        }
+    }
+}
