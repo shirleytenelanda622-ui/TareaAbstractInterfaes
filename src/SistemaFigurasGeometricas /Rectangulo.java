2e14ef4454c8327edd85f90949cd2cf95b1a5c09
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-05-10 16:54:06 -0500

    Add Rectangulo class with area and perimeter methods

--- src/SistemaFigurasGeometricas /Rectangulo.java
+++ src/SistemaFigurasGeometricas /Rectangulo.java
@@ -1,1 +1,19 @@
+public class Rectangulo extends Figura{
+    private double base;
+    private double altura;
 
+    public Rectangulo(String color, boolean rellena, double base, double altura) {
+        super(color, rellena);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea(){
+        return base * altura;
+    }
+    @Override
+    public double calcularPerimetro(){
+        return 2 * (altura + base);
+    }
+}
