034bc3eae78918900753c973215debcaafb15c16
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-05-10 16:53:43 -0500

    Add Circulo class for geometric figure representation

--- src/SistemaFigurasGeometricas /Circulo.java
+++ src/SistemaFigurasGeometricas /Circulo.java
@@ -1,1 +1,17 @@
+public class Circulo extends Figura{
+    private double radio;
 
+    public Circulo(String color, boolean rellena, double radio) {
+        super(color, rellena);
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea(){
+        return 3.14 * (radio * radio);
+    }
+    @Override
+    public double calcularPerimetro(){
+        return 2 * 3.14 *radio;
+    }
+}
