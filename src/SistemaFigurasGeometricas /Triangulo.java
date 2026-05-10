0e184111947594fbddf322d530a4eaff3780fdd4
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-05-10 16:54:25 -0500

    Add Triangulo class for geometric figure representation

--- src/SistemaFigurasGeometricas /Triangulo.java
+++ src/SistemaFigurasGeometricas /Triangulo.java
@@ -1,1 +1,19 @@
+public class Triangulo extends Figura{
+    private double a;
+    private double b;
 
+    public Triangulo(String color, boolean rellena, double a, double b) {
+        super(color, rellena);
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public double calcularArea(){
+        return (a * b) / 2;
+    }
+    @Override
+    public double calcularPerimetro(){
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+}
